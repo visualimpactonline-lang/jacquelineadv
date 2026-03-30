bc8c2ca0cbfafcb00cbbee27c23f68636f6df5d5
Joao Pedro <joaopedro.jpmaia@gmail.com>
2026-03-30 13:24:53 -0300

    ajustes no site

--- src/data/siteContent.js
+++ src/data/siteContent.js
@@ -6,8 +6,8 @@ export const officeInfo = {
   whatsappNumber: '5511999999999',
   whatsappDisplay: '(11) 99999-9999',
   email: 'contato@escritorio.com.br',
-  addressLabel: 'Santa Barbara D'Oeste/SP',
-  mapsUrl: 'https://maps.google.com/?q=Piracicaba+SP',
+  addressLabel: 'Santa Barbara'/SP'
+  mapsUrl: 'https://maps.google.com/?q=Piracicaba+SP'
 };
 
 export const whatsappUrl = `https://wa.me/${officeInfo.whatsappNumber}`;
